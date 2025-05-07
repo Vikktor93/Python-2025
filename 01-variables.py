88b9e37212c236ac065ca79d52b8c3b4a795fb3a
Victor Saldivia <victorsaldivia@outlook.com>
2025-05-07 16:27:18 -0400

    Subiendo archivo de Variables

---
+++ 01-variables.py
@@ -0,0 +1,14 @@
+"""GUIA INICIAL DE PYTHON
+DE VARIABLES - ULAGOS 2025 """
+
+nombre = "Victor"
+apellido = "Saldivia"
+
+# UTILIZANDO VARIABLES EN UN PRINT CON COMAS
+print("Hola mi nombre es" , nombre, "y mi apellido es", apellido)
+
+# IMPRIMIENDO CON OPERADOR DE CONCATENACION
+print("Hola mi nombre es " + nombre + " y mi apellido es " + apellido)	
+
+# IMPRIMIENDO CON F-STRINGS (CADENAS LITERALES)
+print(f"Hola mi nombre es {nombre} y mi apellido es {apellido}")
