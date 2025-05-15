23bb155ba801991ee1590686e8d719206772cb8f
Victor Saldivia <victorsaldivia@outlook.com>
2025-05-15 15:42:45 -0400

    Subiendo archivo de Tipos de Datos

---
+++ 02-tiposdedatos.py
@@ -0,0 +1,17 @@
+# GUIA DE TIPOS DE DATOS EN PYTHON
+# 15 DE MAYO DE 2025
+
+# 01 DECLARANDO E INICIALIZANDO VARIABLES NÚMERICAS
+edad = 31
+estatura  = 1.71
+peso = 75.4
+complejo = 2 + 9j # Inicializando un número complejo
+complejo2 = complex(1, 4) # Otra forma de inicializar un número complejo
+
+# IMPRESIÓN DE NÚMEROS COMPLEJOS
+print(complejo)
+print(complejo2)
+
+# OPERACIÓN ARITMÉTICA
+imc = peso / (estatura ** 2)
+print(f"Su IMC es: {imc}")
