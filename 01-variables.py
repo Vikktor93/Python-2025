339d9e6eafd84dfb72e9f90ee172b7192e085bd1
Victor Saldivia <victorsaldivia@outlook.com>
2025-05-15 15:02:08 -0400

    Subiendo funcion input

--- 01-variables.py
+++ 01-variables.py
@@ -1,18 +1,32 @@
 """GUIA INICIAL DE PYTHON
 DE VARIABLES - ULAGOS 2025 """
 
+# 01 DECLARANDO E INICIALIZANDO VARIABLES
 nombre = "Victor"
 apellido = "Saldivia"
 
-# UTILIZANDO VARIABLES EN UN PRINT CON COMAS
+# 02 IMPRESIÓN DE VARIABLE
+print(nombre)
+
+# 03 UTILIZANDO MÁS DE UNA VARIABLE EN UN PRINT
 print("Hola mi nombre es" , nombre, "y mi apellido es", apellido)
 
-# IMPRIMIENDO CON OPERADOR DE CONCATENACION
+# 04 -IMPRIMIENDO CON OPERADOR DE CONCATENACION
 print("Hola mi nombre es " + nombre + " y mi apellido es " + apellido)	
 
-# IMPRIMIENDO CON F-STRINGS (CADENAS LITERALES)
-print(f"Hola mi nombre es {nombre} y mi apellido es {apellido}")
+# 05 - IMPRIMIENDO CON F-STRINGS (CADENAS LITERALES)
+print(f'Hola mi nombre es {nombre} y mi apellido es {apellido}')
 
-# INICIALIZANDO MULTI VARIABLES EN UNA SOLA LINEA (NO RECOMENDADO)
+# 06 - INICIALIZANDO MULTI VARIABLES EN UNA SOLA LINEA (NO RECOMENDADO)
 ciudad, region, pais = "Castro", "Los Lagos", "Chile"
 print(f"Hola soy de {ciudad}, {region}, {pais}")
+
+peso = 75
+edad = 31
+
+# 07 - USO DEL INPUT / MUTABILIDAD
+print(nombre)
+nombre = input("Ingrese su nombre:\n")
+print(f"Hola mi nombre es: {nombre}")
+
+print(nombre)
